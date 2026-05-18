526b8a5bb0ca58333c404f4ed85ce7f688772e3c
masterDRAGONS <40297635+masterDRAGONS@users.noreply.github.com>
2026-05-18 17:18:55 +0200

    Add HTTPException handling for post retrieval

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -39,3 +39,10 @@ def home(request: Request):
 @app.get("/api/posts")
 def get_posts():
     return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
