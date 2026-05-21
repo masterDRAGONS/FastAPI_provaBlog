61f9f0876cdd4ccab048d480b5abf865d3739379
masterDRAGONS <40297635+masterDRAGONS@users.noreply.github.com>
2026-05-21 17:31:24 +0200

    authentication and authorization implemented

--- main.py
+++ main.py
@@ -119,8 +119,13 @@ async def register_page(request: Request):
         {"title": "Register"},
     )
 
-
-
+@app.get("/account", include_in_schema=False)
+async def account_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "account.html",
+        {"title": "Account"},
+    )
 
 
 @app.exception_handler(StarletteHTTPException)
